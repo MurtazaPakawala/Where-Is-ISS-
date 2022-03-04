fdddfc021c50b8636af23c3ac1e7a48ce3ac930d
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-05 01:37:45 +1100

    everything works perfectlygit add .

---
+++ app.js
@@ -0,0 +1,28 @@
+setInterval(getISS, 2000);
+//   getISS();
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+
+async function getISS() {
+  const response = await fetch(
+    "https://api.wheretheiss.at/v1/satellites/25544"
+  );
+  // console.log(response);
+  const data = await response.json();
+  console.log(data);
+  latitude = data["latitude"];
+  longitude = data["longitude"];
+  console.log(longitude + " " + latitude);
+  document.querySelector("#log").textContent = longitude;
+  document.querySelector("#lat").textContent = latitude;
+  initMap(longitude, latitude);
+}
+let map;
+
+function initMap(lon, lat) {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: lon, lng: lat },
+    zoom: 8,
+  });
+}
+
+console.log("hello everyone is this even working");
